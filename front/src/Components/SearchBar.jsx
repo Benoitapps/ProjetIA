c615d2439ccfa2add02b36ed7f22cd940df94f4d
xxcolas <colas.capel73@gmail.com>
2023-10-14 22:15:56 +0000

    test commit

--- front/src/Components/SearchBar.jsx
+++ front/src/Components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 function SearchBar() {
     const [searchValue, setSearchValue] = useState("");
     const handleClick = () => {
-        console.log(searchValue);
+        console.log("input : " + searchValue);
     }
 
     return (
