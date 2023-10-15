bb9e32aa2e202f0983809f60ce999309c9aab8a3
decarli <benoitdecarli77@gmail.com>
2023-10-15 13:03:30 +0200

    part recette ok

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const recipe = require('../controllers/recipe');
 
-router.post('/', recipe.getRecipe);
+router.post('/', recipe.getRecipeVerif);
 
 module.exports = router;
