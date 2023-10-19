7f4c7bb9e7eb47153a043411e5323e19b2c35ea9
xxcolas <colas.capel73@gmail.com>
2023-10-19 19:01:20 +0000

    :wrench: add react router dom

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
 import './App.css';
-import SearchBar from './Components/Search/SearchBar';
-import RecipesList from './Components/Search/RecipesList';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from './Components/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/recipe",
+    element: <div>Page des recettes</div>,
+  },
+]);
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
-
   return (
     <>
-      <SearchBar setRecipes={setRecipes}/>
-      <RecipesList recipes={recipes}/>
+      <RouterProvider router={router} />
     </>
   )
 }
