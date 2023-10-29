8413e48064218b3e696434d1b0ba903eb3e6670c
MelvinCourant <melvin.courant1@gmail.com>
2023-10-29 10:41:55 +0100

    :lipstick: add style to search bar and hero

--- front/src/Components/Home.jsx
+++ front/src/Components/Home.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import SearchBar from './Search/SearchBar';
 import RecipesList from './Search/RecipesList';
+import '@css/Home.css';
 
 function HomePage() {
     const [recipes, setRecipes] = useState([]);
 
     return (
         <>
-            <SearchBar setRecipes={setRecipes}/>
+            <div className="hero">
+                <SearchBar setRecipes={setRecipes}/>
+            </div>
             <RecipesList recipes={recipes}/>
         </>
     )
