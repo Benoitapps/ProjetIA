b1b127bb3650243bd4e7708a0068e624b0961796
xxcolas <colas.capel73@gmail.com>
2023-12-14 21:41:20 +0000

    WIP

--- backend/controllers/search.js
+++ backend/controllers/search.js
@@ -1,5 +1,7 @@
 const bot = require("../bot/bot").bot;
 
+const { Recipe, Ingredient } = require("../db");
+
 require("dotenv").config({ path: ".env.local", override: true });
 
 async function getRecipes(req, res) {
@@ -15,7 +17,16 @@ async function getRecipes(req, res) {
             et tu devras répondre à toutes les questions que je te poserai en tant que chef étoilé.
         `;
 
-        const question = `
+        console.log("before recipes-----------------------------------------")
+        const recipes = await Recipe.findAll({
+            include: [{
+                model: Ingredient,
+            }],
+        });
+        console.log("after recipes-----------------------------------------")
+        console.log(recipes)
+
+        const question_old = `
             Je veux, dans un premier temps, que tu me donnes la description (moins de 300 caractères) et le temps de préparation total de cette recette : ${req.body.recipe}.
             Je veux dans un second temps, que tu me donnes 3 autres recettes (avec l'ingrédient principal en commun)
             qui sont similaires à celle que je t'ai donné, avec une description (moins de 300 caractères) et le temps de préparation total.
@@ -45,8 +56,8 @@ async function getRecipes(req, res) {
             Je ne veux pas d'autres informations inutiles, je veux que ta réponse ne soit que du JSON.
         `;
 
-        const answer = await bot(contextBot, question);
-        const answerJsonObject = JSON.parse(answer);
+        // const answer = await bot(contextBot, question);
+        const answerJsonObject = JSON.parse(recipes);
 
         res.status(200).json(answerJsonObject);
 
---
+++ backend/db/Fixtures/requete.sql
@@ -0,0 +1,253 @@
+INSERT INTO recipes (name, description, preparation)
+VALUES
+    ('Tarte aux pommes', 'Délicieuse tarte aux pommes faite maison.', 'Préchauffez le four à 180°C. Épluchez et coupez les pommes en tranches fines. Disposez-les sur la pâte brisée. Saupoudrez de sucre et de cannelle. Enfournez pendant 30 minutes.'),
+    ('Poulet rôti', 'Poulet rôti au four avec des herbes et des légumes.', 'Préchauffez le four à 200°C. Assaisonnez le poulet avec du sel, du poivre et des herbes de Provence. Placez-le dans un plat avec des pommes de terre, des carottes et des oignons. Enfournez pendant 1 heure.'),
+    ('Pâtes Carbonara', 'Pâtes avec une sauce crémeuse à base d''œufs, de fromage et de lardons.', 'Faites cuire les pâtes al dente. Pendant ce temps, faites revenir les lardons dans une poêle. Dans un bol, mélangez les œufs, le parmesan et du poivre. Ajoutez la sauce aux pâtes cuites et mélangez.'),
+    ('Salade César', 'Salade verte avec du poulet grillé, des croûtons et une sauce César.', 'Préparez une vinaigrette à base de moutarde, d''ail, de parmesan, de jus de citron, d''huile d''olive, de sel et de poivre. Disposez la laitue dans un saladier, ajoutez du poulet grillé, des croûtons et la vinaigrette.'),
+    ('Lasagnes', 'Lasagnes aux couches de viande, de pâtes et de fromage.', 'Faites cuire les pâtes à lasagne. Préparez une sauce bolognaise avec de la viande hachée, des tomates et des épices. Dans un plat, alternez les couches de pâtes, de sauce et de fromage. Enfournez pendant 25 minutes.'),
+    ('Risotto aux champignons', 'Risotto crémeux aux champignons et au parmesan.', 'Faites revenir des champignons dans du beurre. Ajoutez du riz arborio et faites-le cuire avec du bouillon de poulet. Ajoutez du parmesan et du persil. Remuez jusqu''à ce que le risotto soit crémeux.'),
+    ('Sushi', 'Rouleaux de riz, de poisson et de légumes enveloppés dans des algues.', 'Cuisez le riz à sushi. Disposez une feuille de nori sur une natte de bambou. Étalez le riz sur la moitié de la feuille. Ajoutez des morceaux de poisson, d''avocat et de concombre. Roulez et coupez en tranches.'),
+    ('Burger classique', 'Burger avec un steak de bœuf, des légumes et du fromage.', 'Faites cuire un steak de bœuf selon vos préférences. Assemblez le burger avec du pain, du steak, des tomates, de la laitue, du fromage et de la sauce. Servez avec des frites.'),
+    ('Gâteau au chocolat', 'Délicieux gâteau au chocolat moelleux.', 'Faites fondre le chocolat et le beurre. Battez les œufs avec le sucre. Ajoutez le mélange chocolat-beurre et la farine. Versez dans un moule et cuisez au four à 180°C pendant 25 minutes.'),
+    ('Salade de quinoa', 'Salade saine avec du quinoa, des légumes et une vinaigrette légère.', 'Cuisez le quinoa et laissez-le refroidir. Mélangez-le avec des légumes tels que des tomates, des concombres et des poivrons. Assaisonnez avec une vinaigrette à l''huile d''olive et au citron.'),
+    ('Pizza margherita', 'Pizza classique avec de la sauce tomate, de la mozzarella et du basilic.', 'Étalez de la pâte à pizza sur une plaque. Étalez de la sauce tomate, ajoutez de la mozzarella et des feuilles de basilic. Cuisez au four à 220°C pendant 15 minutes.'),
+    ('Poisson grillé', 'Poisson grillé avec des herbes et du citron.', 'Assaisonnez le poisson avec du sel, du poivre, des herbes fraîches et du jus de citron. Faites-le griller sur un barbecue ou au four. Servez avec des légumes grillés.'),
+    ('Soupe de tomates', 'Soupe chaude et réconfortante à base de tomates.', 'Faites revenir des oignons et de l''ail dans de l''huile d''olive. Ajoutez des tomates, du bouillon de légumes et des herbes. Laissez mijoter jusqu''à ce que les tomates soient tendres. Mixez la soupe.'),
+    ('Curry de poulet', 'Curry de poulet épicé avec du lait de coco et des légumes.', 'Faites revenir du poulet, des oignons et des épices dans une casserole. Ajoutez du lait de coco et des légumes. Laissez mijoter jusqu''à ce que le poulet soit cuit et la sauce épaissie.'),
+    ('Smoothie aux fruits', 'Smoothie rafraîchissant avec des fruits mélangés.', 'Mélangez des fruits tels que des bananes, des fraises et des oranges dans un blender. Ajoutez du yaourt et du miel. Mixez jusqu''à obtention d''une consistance lisse.'),
+    ('Gaspacho', 'Soupe froide à base de tomates, de concombres et de poivrons.', 'Mixez des tomates, des concombres, des poivrons, de l''ail et du vinaigre dans un blender. Assaisonnez avec du sel et du poivre. Réfrigérez avant de servir.'),
+    ('Pancakes', 'Pancakes moelleux servis avec du sirop d''érable.', 'Mélangez de la farine, du sucre, de la levure chimique, du lait et des œufs pour faire la pâte à pancakes. Cuisez les pancakes dans une poêle chaude. Servez avec du sirop d''érable.'),
+    ('Ratatouille', 'Ratatouille provençale avec des légumes d''été.', 'Coupez des légumes tels que des courgettes, des aubergines et des poivrons. Faites-les cuire dans une poêle avec de l''huile d''olive, de l''ail et des herbes. Servez chaud.'),
+    ('Tiramisu', 'Dessert italien avec des couches de biscuits imbibés de café et de mascarpone.', 'Trempez des biscuits dans du café fort. Disposez-les dans un plat. Mélangez du mascarpone, des œufs, du sucre et de l''extrait de vanille. Alternez les couches de biscuits et de crème. Réfrigérez.'),
+    ('Chili con carne', 'Chili épicé avec de la viande hachée, des haricots et des tomates.', 'Faites revenir de la viande hachée, des oignons et de l''ail dans une casserole. Ajoutez des haricots rouges, des tomates en dés, du chili en poudre et du cumin. Laissez mijoter jusqu''à épaississement.'),
+    ('Margarita', 'Cocktail classique à base de tequila, de triple sec et de jus de citron vert.', 'Mélangez de la tequila, du triple sec et du jus de citron vert dans un shaker avec de la glace. Versez dans un verre à margarita avec du sel sur le bord. Décorez avec une tranche de citron vert.'),
+    ('Fondue au fromage', 'Fondue suisse avec du fromage fondu et du pain.', 'Frottez l''intérieur d''un caquelon avec de l''ail. Ajoutez du vin blanc et chauffez. Ajoutez du fromage suisse râpé et remuez jusqu''à ce qu''il soit fondu. Servez avec du pain.'),
+    ('Moussaka', 'Plat grec avec des couches d''aubergines, de viande hachée et de béchamel.', 'Faites cuire des aubergines tranchées. Faites revenir de la viande hachée avec des oignons et de l''ail. Alternez les couches d''aubergines et de viande dans un plat. Ajoutez une couche de béchamel. Cuisez au four.'),
+    ('Gâteau au fromage', 'Gâteau au fromage crémeux avec une croûte de biscuits.', 'Mélangez du fromage à la crème, du sucre, des œufs et de la vanille. Versez sur une croûte de biscuits écrasés. Cuisez au four à 160°C pendant 45 minutes. Laissez refroidir avant de servir.');
+
+-- Ajout d'ingrédients pour la recette de la Tarte aux pommes
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Pâte brisée', 1),
+  ('Pommes', 1),
+  ('Sucre', 1),
+  ('Cannelle', 1);
+
+-- Ajout d'ingrédients pour la recette du Poulet rôti
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Poulet', 2),
+  ('Sel', 2),
+  ('Poivre', 2),
+  ('Herbes de Provence', 2),
+  ('Pommes de terre', 2),
+  ('Carottes', 2),
+  ('Oignons', 2);
+
+-- Ajout d'ingrédients pour la recette des Pâtes Carbonara
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Pâtes', 3),
+  ('Œufs', 3),
+  ('Fromage', 3),
+  ('Lardons', 3);
+
+-- Ajout d'ingrédients pour la recette de la Salade César
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Laitue', 4),
+  ('Poulet grillé', 4),
+  ('Croûtons', 4),
+  ('Moutarde', 4),
+  ('Ail', 4),
+  ('Parmesan', 4),
+  ('Jus de citron', 4),
+  ('Huile d''olive', 4),
+  ('Sel', 4),
+  ('Poivre', 4);
+
+-- Ajout d'ingrédients pour la recette des Lasagnes
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Pâtes à lasagne', 5),
+  ('Viande hachée', 5),
+  ('Tomates', 5),
+  ('Épices', 5),
+  ('Fromage', 5);
+
+-- Ajout d'ingrédients pour la recette du Risotto aux champignons
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Champignons', 6),
+  ('Beurre', 6),
+  ('Riz arborio', 6),
+  ('Bouillon de poulet', 6),
+  ('Parmesan', 6),
+  ('Persil', 6);
+
+-- Ajout d'ingrédients pour la recette du Sushi
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Riz à sushi', 7),
+  ('Poisson', 7),
+  ('Avocat', 7),
+  ('Concombre', 7),
+  ('Feuilles de nori', 7);
+
+-- Ajout d'ingrédients pour la recette du Burger classique
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Steak de bœuf', 8),
+  ('Pain à burger', 8),
+  ('Tomates', 8),
+  ('Laitue', 8),
+  ('Fromage', 8),
+  ('Sauce à burger', 8);
+
+-- Ajout d'ingrédients pour la recette du Gâteau au chocolat
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Chocolat', 9),
+  ('Beurre', 9),
+  ('Œufs', 9),
+  ('Sucre', 9),
+  ('Farine', 9);
+
+-- Ajout d'ingrédients pour la recette de la Salade de quinoa
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Quinoa', 10),
+  ('Tomates', 10),
+  ('Concombres', 10),
+  ('Poivrons', 10),
+  ('Vinaigrette', 10);
+
+-- Ajout d'ingrédients pour la recette de la Pizza margherita
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Pâte à pizza', 11),
+  ('Sauce tomate', 11),
+  ('Mozzarella', 11),
+  ('Basilic', 11);
+
+-- Ajout d'ingrédients pour la recette du Poisson grillé
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Poisson', 12),
+  ('Sel', 12),
+  ('Poivre', 12),
+  ('Herbes fraîches', 12),
+  ('Citron', 12);
+
+-- Ajout d'ingrédients pour la recette de la Soupe de tomates
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Oignons', 13),
+  ('Ail', 13),
+  ('Tomates', 13),
+  ('Bouillon de légumes', 13),
+  ('Herbes', 13);
+
+-- Ajout d'ingrédients pour la recette du Curry de poulet
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Poulet', 14),
+  ('Oignons', 14),
+  ('Épices', 14),
+  ('Lait de coco', 14),
+  ('Légumes', 14);
+
+-- Ajout d'ingrédients pour la recette du Smoothie aux fruits
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Bananes', 15),
+  ('Fraises', 15),
+  ('Oranges', 15),
+  ('Yaourt', 15),
+  ('Miel', 15);
+
+-- Ajout d'ingrédients pour la recette du Gaspacho
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Tomates', 16),
+  ('Concombres', 16),
+  ('Poivrons', 16),
+  ('Ail', 16),
+  ('Vinaigre', 16);
+
+
+-- Ajout d'ingrédients pour la recette des Pancakes
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Farine', 17),
+  ('Sucre', 17),
+  ('Levure chimique', 17),
+  ('Lait', 17),
+  ('Œufs', 17);
+
+-- Ajout d'ingrédients pour la recette de la Ratatouille
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Courgettes', 18),
+  ('Aubergines', 18),
+  ('Poivrons', 18),
+  ('Ail', 18),
+  ('Herbes', 18);
+
+-- Ajout d'ingrédients pour la recette du Tiramisu
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Biscuits', 19),
+  ('Café', 19),
+  ('Mascarpone', 19),
+  ('Œufs', 19),
+  ('Sucre', 19),
+  ('Extrait de vanille', 19);
+
+-- Ajout d'ingrédients pour la recette du Chili con carne
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Viande hachée', 20),
+  ('Oignons', 20),
+  ('Haricots rouges', 20),
+  ('Tomates en dés', 20),
+  ('Chili en poudre', 20),
+  ('Cumin', 20);
+
+-- Continuez à ajouter des ingrédients pour les autres recettes
+-- ...
+
+-- Ajout d'ingrédients pour la recette de la Margarita
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Tequila', 21),
+  ('Triple sec', 21),
+  ('Jus de citron vert', 21),
+  ('Glaçons', 21),
+  ('Sel', 21);
+
+-- Ajout d'ingrédients pour la recette de la Fondue au fromage
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Fromage suisse', 22),
+  ('Ail', 22),
+  ('Vin blanc', 22),
+  ('Pain', 22);
+
+-- Ajout d'ingrédients pour la recette de la Moussaka
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Aubergines', 23),
+  ('Viande hachée', 23),
+  ('Oignons', 23),
+  ('Ail', 23),
+  ('Béchamel', 23);
+
+-- Ajout d'ingrédients pour la recette du Gâteau au fromage
+INSERT INTO ingredients (name, recipe_id)
+VALUES
+  ('Fromage à la crème', 24),
+  ('Sucre', 24),
+  ('Œufs', 24),
+  ('Vanille', 24),
+  ('Biscuits écrasés', 24);
