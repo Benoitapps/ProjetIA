d7917d34abf2c18a54da00d5b85a7a48bdcf81c5
xxcolas <colas.capel73@gmail.com>
2023-12-17 20:16:33 +0000

    search recipe with powered IA

--- front/src/Components/Search/RecipesList.jsx
+++ front/src/Components/Search/RecipesList.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 import RecipeCard from './RecipeCard';
 import '@css/Search/RecipesList.css';
 
-function RecipesList({ recipes }) {
-    return (
-        <>
-          <section className="recipes-list">
-            <h2 className="recipes-list-title">Résultats de la recherche</h2>
-            <ul className="recipes-list-container">
-              {
-                recipes.map((recipe, index) => {
-                  return <li><RecipeCard key={index} recipe={recipe} /></li>
-                })
-              }
-            </ul>
-          </section>
-        </>
-    );
+function RecipesList({ recipes, isLoading }) {
+  return (
+    <>
+      <section className="recipes-list">
+        <h2 className="recipes-list-title">Résultats de la recherche</h2>
+        {
+          isLoading
+            ? <p className="recipes-list-loading">Chargement des recettes...</p>
+            : recipes.length === 0
+              ? <p className="recipes-list-empty">Aucune recette ne correspond à votre recherche...</p>
+              :
+              <ul className="recipes-list-container">
+                {
+                  recipes.map((recipe, index) => {
+                    return <li key={index}>
+                      <RecipeCard recipe={recipe} />
+                    </li>
+                  })
+                }
+              </ul>
+        }
+      </section>
+    </>
+  );
 }
 
 export default RecipesList;
