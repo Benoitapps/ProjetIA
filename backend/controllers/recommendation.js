0291798fdcc191f2da0f61e9638cc5b3074e08fb
decarli <benoitdecarli77@gmail.com>
2024-01-02 16:24:56 +0100

    ajour preference dans le back pour recommendation

--- backend/controllers/recommendation.js
+++ backend/controllers/recommendation.js
@@ -5,6 +5,7 @@ const Image = require("../db").Image;
 const FoodPreference = require("../db").FoodPreference;
 const bot = require("../bot/bot").bot;
 
+
 async function getRecomendation(req, res) {
 
     try {
@@ -14,13 +15,9 @@ async function getRecomendation(req, res) {
         const recipeId = req.params.recipeId;
 
         //la recette selectionner
-        let myRecipe;
-        try {
-            myRecipe = await Recipe.findOne({ where: { id: recipeId } });
+          let  myRecipe = await Recipe.findOne({ where: { id: recipeId } });
             // console.log("myrecipe.name", myRecipe.name);
-        } catch (error) {
-            return res.status(500).json({ error: error.message });
-        }
+
 
         //toutes les recettes sauf celle selectionner
         let recipes;
@@ -35,21 +32,67 @@ async function getRecomendation(req, res) {
         } catch (error) {
             return res.status(500).json({ error: error.message });
         }
+        async function getInge(recipes){
+            const recipesWithIngredients = [];
 
-        //toutes les recettes chosis par IA
-        const allrecipe = recipes.map((recipe) => {
-            return {
-                id: recipe.id,
-                name: recipe.name,
-                description: recipe.description,
-                src: recipe.src,
-            };
-        });
-        const tabRecipe = allrecipe.map((item) => `${item.id}:${item.name}:${item.src}:${item.description}`).join(', ');
+            for (const recipe of recipes) {
+                const ingredients = await recipe.getIngredients();
+                console.log("ingredients",ingredients)
+
+                const recipeWithIngredients = {
+                    id: recipe.id,
+                    name: recipe.name,
+                    description: recipe.description,
+                    src: recipe.src,
+                    ingredients: ingredients.map(ingredient => ({
+                        name: ingredient.name,
+                    })),
+                };
+                // console.log("recipeWithIngredients",recipeWithIngredients)
+                recipesWithIngredients.push(recipeWithIngredients);
+            }
+            console.log("recipesWithIngredients");
+            return recipesWithIngredients;
+        }
+
+        //toutes les recettes avec leurs ingredients
+        let recipeWithIngredients = await getInge(recipes);
 
 
-        //Profil de l'IA
-        const profilBot = `profilBotn Voici les recettes ainsi que leurs id qui sont issues de ma base de données.
+        // console.log("allrecipe", allrecipe)
+        const tabRecipe = recipeWithIngredients.map((item) => `${item.id}:${item.name}: ${item.src} :${item.ingredients.map(ing => ing.name).join(', ')}`).join(', ');
+         console.log("tabRecipe", tabRecipe)
+
+        let profilBot;
+
+        if(req.cookies.token){
+            console.log("IL Y A UN TOKEN")
+            let token = req.cookies.token;
+            const userId = await getConnectedUser(token);
+            console.log("userId", userId)
+
+            const preferences = await FoodPreference.findAll({where: {user_id: userId}});
+            const allPreferences = preferences.map(preference => preference.name);
+            const allPreferencesString = allPreferences.join(', ');
+            console.log("allPreferencesString", allPreferencesString)
+
+            profilBot = `profilBotn Voici les recettes ainsi que leurs id qui sont issues de ma base de données.
+            Les données sont écrite de cette facon identifiant:nom_de_la_recette.
+
+            voici les recettes de ma base de données:  ${tabRecipe}
+
+        J'aimerais que pour cette recette: ${myRecipe}, tu me donnes uniquement quatre recettes qui se rapproche le plus possible en fonction des ingrédients qui la compose les recette ne doivent pas contenir les ingredient : ${allPreferencesString}.
+
+        Si il y a moins de quatre recettes similaire tiré de ma base de données, tu m'ajouteras le nombre nécessaire de recette pour en avoir quatre au total. 
+
+        retournes UNIQUEMENT et seulement un objet JSON et pas d'autres textes, le JSON aura cette forme :
+            [{"id": 11,"recette": "crepe au chocolat","src":"liens"},{"id": 17,"recette": "Pain au chocolat","src":"liens"},{"id": 14,"recette": "gateau a la fraise","src":"liens"},{"id": 16,"recette": "crepe au sucre","src":"liens"}]`;
+
+        }else {
+
+
+            //Profil de l'IA
+            profilBot = `profilBotn Voici les recettes ainsi que leurs id qui sont issues de ma base de données.
             Les données sont écrite de cette facon identifiant:nom_de_la_recette.
 
             voici les recettes de ma base de données:  ${tabRecipe}
@@ -61,6 +104,9 @@ async function getRecomendation(req, res) {
         retournes UNIQUEMENT et seulement un objet JSON et pas d'autres textes, le JSON aura cette forme :
             [{"id": 11,"recette": "crepe au chocolat","src":"liens"},{"id": 17,"recette": "Pain au chocolat","src":"liens"},{"id": 14,"recette": "gateau a la fraise","src":"liens"},{"id": 16,"recette": "crepe au sucre","src":"liens"}]`;
 
+        }
+
+        console.log("profilBot", profilBot)
         //reponse de l'IA
         answer = await getAnswer(tabRecipe, myRecipe, profilBot);
 
@@ -79,8 +125,8 @@ async function getRecomendation(req, res) {
         });
 
          //les differentes recettes
-        console.log("recipeIds", recipeIds);
-        console.log("answerJson", answerJson);
+        // console.log("recipeIds", recipeIds);
+        // console.log("answerJson", answerJson);
         // console.log("recipe", recipe)
 
 
