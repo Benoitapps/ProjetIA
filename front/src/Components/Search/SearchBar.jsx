be4ba6757b389a06c6940c9ebc9abce4f0e73185
MelvinCourant <melvin.courant1@gmail.com>
2024-01-08 16:07:38 +0100

    :sparkles: Add vocal research with search bar

--- front/src/Components/Search/SearchBar.jsx
+++ front/src/Components/Search/SearchBar.jsx
@@ -2,22 +2,21 @@ import React, { useState, useEffect } from 'react';
 import searchRecipe from '../../hook/search';
 import '@css/Search/SearchBar.css';
 import loading from '@img/loading.gif';
+import 'regenerator-runtime';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 function SearchBar({ setRecipes, setIsLoading, isLoading }) {
     const [searchValue, setSearchValue] = useState("");
     const [error, setError] = useState(null);
+    const [isListening, setIsListening] = useState(false);
 
-    const handleClick = async () => {
-        if (searchValue.trim() === "") {
-            return;
-        }
-
+    const makeRequest = async (value) => {
         setIsLoading(true);
         setError(null);
 
         try {
             setRecipes([]);
-            const recipes = await searchRecipe(searchValue);
+            const recipes = await searchRecipe(value);
             setRecipes(recipes);
         } catch (err) {
             setError("Une erreur s'est produite lors de la recherche.");
@@ -26,11 +25,39 @@ function SearchBar({ setRecipes, setIsLoading, isLoading }) {
         setIsLoading(false);
     }
 
+    const handleClick = async () => {
+        await makeRequest(searchValue);
+    }
+
     useEffect(() => {
         if (!isLoading) {
             setIsLoading(false);
         }
     }, [isLoading]);
+
+    const {
+        transcript,
+        resetTranscript,
+        listening,
+    } = useSpeechRecognition();
+
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+        console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
+    }
+
+    const handleListen = async () => {
+        if (!listening) {
+            SpeechRecognition.startListening({ language: 'fr-FR'});
+            setIsListening(true);
+            setSearchValue("");
+            resetTranscript();
+        } else {
+            SpeechRecognition.stopListening();
+            setIsListening(false);
+            setSearchValue(transcript);
+            await makeRequest(transcript);
+        }
+    }
 
     return (
         <>
@@ -51,7 +78,11 @@ function SearchBar({ setRecipes, setIsLoading, isLoading }) {
                     value={searchValue}
                     onChange={e => setSearchValue(e.target.value)}
                 />
-                <button>
+                <button
+                    type="button"
+                    onClick={handleListen}
+                    className={`micro${isListening ? "--listening" : ""}`}
+                >
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 26" fill="none">
                         <path d="M17.9688 11.4375V13.7812C17.9687 15.2317 17.3926 16.6227 16.367 17.6482C15.3414 18.6738 13.9504 19.25 12.5 19.25C11.0496 19.25 9.6586 18.6738 8.63301 17.6482C7.60742 16.6227 7.03125 15.2317 7.03125 13.7812V11.4375H5.46875V13.7812C5.46957 15.5102 6.10739 17.1782 7.26033 18.4667C8.41327 19.7551 10.0005 20.5735 11.7188 20.7656V22.375H8.59375V23.9375H16.4062V22.375H13.2812V20.7656C14.9995 20.5735 16.5867 19.7551 17.7397 18.4667C18.8926 17.1782 19.5304 15.5102 19.5312 13.7812V11.4375H17.9688Z" fill="#989898"/>
                         <path d="M12.5 17.6875C13.013 17.6875 13.5209 17.5865 13.9949 17.3902C14.4688 17.1938 14.8994 16.9061 15.2621 16.5434C15.6249 16.1807 15.9126 15.75 16.1089 15.2761C16.3052 14.8022 16.4062 14.2942 16.4062 13.7812V5.96875C16.4062 4.93275 15.9947 3.93918 15.2621 3.20661C14.5296 2.47405 13.536 2.0625 12.5 2.0625C11.464 2.0625 10.4704 2.47405 9.73786 3.20661C9.0053 3.93918 8.59375 4.93275 8.59375 5.96875V13.7812C8.59375 14.2942 8.69479 14.8022 8.8911 15.2761C9.0874 15.75 9.37514 16.1807 9.73786 16.5434C10.4704 17.2759 11.464 17.6875 12.5 17.6875ZM10.1562 5.96875C10.1562 5.34715 10.4032 4.75101 10.8427 4.31147C11.2823 3.87193 11.8784 3.625 12.5 3.625C13.1216 3.625 13.7177 3.87193 14.1573 4.31147C14.5968 4.75101 14.8438 5.34715 14.8438 5.96875V13.7812C14.8438 14.4029 14.5968 14.999 14.1573 15.4385C13.7177 15.8781 13.1216 16.125 12.5 16.125C11.8784 16.125 11.2823 15.8781 10.8427 15.4385C10.4032 14.999 10.1562 14.4029 10.1562 13.7812V5.96875Z" fill="#989898"/>
